5389a04850586dc5249961dbb0aab3be308edf84
Jamie <gatorjuice@gmail.com>
2016-06-07 20:39:17 -0500

    created create method for Punches and assigned redirects

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -1,7 +1,25 @@
 class PunchesController < ApplicationController
+  def index
+    if current_user
+      @user = current_user
+      @punches = current_user.punches if current_user.punches
+    else
+      redirect_to '/users/sign_in'
+    end
+  end
 
-	def index
-		@user = current_user
-		@punches = current_user.punches
-	end
+  def create
+    input_event_type = params[:event_type]
+    punch = Punch.new(
+      event_type: input_event_type,
+      user_id: current_user.id
+    )
+    if punch.save
+      flash[:punch_message] = "#{punch.event_type} successfull." 
+      sign_out :user
+      redirect_to '/users/sign_in'
+    else
+      render 'index.html.erb'
+    end
+  end
 end
