4acc28ffab51aeb620ab9e491c91dcd390949ec3
jgates <jgates@leapfrogonline.com>
2016-06-15 15:10:55 -0500

    styled flash messages and made app log user out after succesful punch

--- app/controllers/punches_controller.rb
+++ app/controllers/punches_controller.rb
@@ -1,4 +1,7 @@
 class PunchesController < ApplicationController
+
+  before_action :active_user?
+
   def index
     @user = current_user
     if current_user.punches.any? && current_user.punches.last.clock_out.nil?
@@ -12,16 +15,28 @@ class PunchesController < ApplicationController
 
   def create
     if params[:clock_in]
-      Punch.create(
+      punch = Punch.create(
         user_id: current_user.id,
         clock_in: Time.zone.now
       )
-      redirect_to '/'
+      flash[:punch_message] = "#{current_user.email}, clock in successful."
+      sign_out :user
+      redirect_to '/users/sign_in'
     elsif params[:clock_out]
       current_user.punches.last.update(
         clock_out: Time.zone.now
       )
-      redirect_to '/'
+      flash[:punch_message] = "#{current_user.email}, clock out successful."
+      sign_out :user
+      redirect_to '/users/sign_in'
+    end
+  end
+
+  private
+
+  def active_user?
+    unless current_user
+      redirect_to '/users/sign_in'
     end
   end
 end
